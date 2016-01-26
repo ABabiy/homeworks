a0846610470837e9b86e9793fc58dd211dc8fdc4
ABabiy <ababiy@gmail.com>
2016-01-26 03:33:18 +0200

    Commit 22 урока

--- index.php
+++ index.php
@@ -7,57 +7,33 @@
 	 */
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>РНР Академия. Дамашние задания</title>
+<script>
+	document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
+</script>
 
-	<link href="css/mycss.css" rel="stylesheet">
+<?php
 
-	<script>
-		document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
-	</script>
+/*Урок 22. Константы
 
-</head>
-<body>
-
-<div class="wrapper">
-
-	<h2>Урок 22. Константы</h2>
-
-	<h3>Определить константы, которые соответствуют названиям нескольких стран мира.
-		Используя эти константы, сформировать массив из соответствующих значений</h3>
-
-	<p>
-		define('UKRAINE', 'Киев'); <br>
-		define('RUSSIAN', 'Москва'); <br>
-		define('FRANCE', 'Париж'); <br>
-	</p>
+Определить константы, которые соответствуют названиям нескольких стран мира.
+Используя эти константы, сформировать массив из соответствующих значений*/
 
 
-	<h3>Код</h3>
+	define('UKRAINE', 'Киев');
+	define('RUSSIAN', 'Москва');
+	define('FRANCE', 'Париж');
 
-	<p>
-		<code>
-			$sity = array(UKRAINE, RUSSIAN, FRANCE);<br>
-			var_dump($sity);<br>
-		</code>
-	</p>
+	$sity = array(UKRAINE, RUSSIAN, FRANCE);
 
-	<h3>Результат</h3>
+	var_dump($sity);
 
-	<?php
+/*Урок 23. Переменные переменных
 
-		define('UKRAINE', 'Киев');
-		define('RUSSIAN', 'Москва');
-		define('FRANCE', 'Париж');
+Создать скрипт, который бы вывел на экран значение переменной $$$$$hello.
+Значение может быть любым.*/
 
-		$sity = array(UKRAINE, RUSSIAN, FRANCE);
 
-		var_dump($sity);
-	?>
-</div>
 
-</body>
-</html>
+
+
+?>
